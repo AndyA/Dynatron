fbf5be0eb7cd162039f9120e2e768b06955150f0
Andy Armstrong <andy@hexten.net>
2013-05-08 18:21:05 +0100

    Test coverage for queues.

---
+++ t/queue.c
@@ -0,0 +1,42 @@
+/* queue.t */
+
+#include "util.h"
+#include "tap.h"
+#include "jd_test.h"
+
+#include "jd_pretty.h"
+#include "dynatron.h"
+
+static void test_queue(void) {
+  scope {
+    dy_queue *q;
+    jd_var *msg = jd_nhv(1), *got = jd_nv();
+    int i;
+
+    q = dy_queue_new();
+
+    for (i = 0; i < 5; i++) {
+      jd_set_int(jd_get_ks(msg, "sn", 1), i);
+      dy_queue_enqueue(q, msg);
+    }
+
+    is(dy_queue_available(q), 5, "5 items in queue");
+
+    for (i = 0; i < 5; i++) {
+      jd_set_int(jd_get_ks(msg, "sn", 1), i);
+      dy_queue_dequeue(q, got);
+      jdt_is(got, msg, "message %d", i);
+    }
+
+    is(dy_queue_available(q), 0, "0 items in queue");
+
+    dy_queue_free(q);
+  }
+}
+
+void test_main(void) {
+  test_queue();
+}
+
+/* vim:ts=2:sw=2:sts=2:et:ft=c
+ */
